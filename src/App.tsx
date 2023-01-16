91c17de7bb17295bf8e99d3726268e67a10629db
nobody <hjing0524@gamil.com>
2023-01-16 20:51:08 +0800

    feat: App初始组件

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './index.scss';
+import * as React from 'react';
+import { useState } from 'react';
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div className="container">
+      hello react-ts-webpack demo {counter} clicks
+      <button
+        onClick={handleClick}
+      >
+        press
+      </button>
+    </div>
+  );
+}
